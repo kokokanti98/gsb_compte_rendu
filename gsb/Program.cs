836ca9e7d921c6d393fced17a74c7e9f3b6e7a14
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2020-06-18 21:41:25 +0200

    Add files via upload

---
+++ gsb/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gbs_gcr1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 f1 = new Form1();
+            f1.Show();
+            Application.Run();
+        }
+    }
+}
