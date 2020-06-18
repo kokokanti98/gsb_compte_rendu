ab18c18eda3865a4a1540cc1ef8b9ba59fd72a14
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2020-06-18 21:42:23 +0200

    Add files via upload

---
+++ gsb/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace gbs_gcr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelLogo1 = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tbMdp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblErrorconnexion = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogo1
+            // 
+            this.panelLogo1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panelLogo1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLogo1.BackgroundImage")));
+            this.panelLogo1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLogo1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLogo1.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo1.Name = "panelLogo1";
+            this.panelLogo1.Size = new System.Drawing.Size(351, 760);
+            this.panelLogo1.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnLogin.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogin.Font = new System.Drawing.Font("Garamond", 13.875F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLogin.Location = new System.Drawing.Point(351, 679);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(841, 81);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Connexion";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 436F));
+            this.tableLayoutPanel1.Controls.Add(this.tbMdp, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(351, 599);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(841, 80);
+            this.tableLayoutPanel1.TabIndex = 4;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // tbMdp
+            // 
+            this.tbMdp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbMdp.Font = new System.Drawing.Font("Garamond", 13.875F);
+            this.tbMdp.Location = new System.Drawing.Point(408, 3);
+            this.tbMdp.Multiline = true;
+            this.tbMdp.Name = "tbMdp";
+            this.tbMdp.Size = new System.Drawing.Size(430, 74);
+            this.tbMdp.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Garamond", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label1.Location = new System.Drawing.Point(179, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mot de passe";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 436F));
+            this.tableLayoutPanel2.Controls.Add(this.tbLogin, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(351, 519);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(841, 80);
+            this.tableLayoutPanel2.TabIndex = 5;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLogin.Font = new System.Drawing.Font("Garamond", 13.875F);
+            this.tbLogin.Location = new System.Drawing.Point(408, 3);
+            this.tbLogin.Multiline = true;
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(430, 74);
+            this.tbLogin.TabIndex = 0;
+            this.tbLogin.TextChanged += new System.EventHandler(this.tbLogin_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Font = new System.Drawing.Font("Garamond", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label2.Location = new System.Drawing.Point(219, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 80);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Identifiant";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblErrorconnexion);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(351, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(841, 100);
+            this.panel1.TabIndex = 6;
+            // 
+            // lblErrorconnexion
+            // 
+            this.lblErrorconnexion.AutoSize = true;
+            this.lblErrorconnexion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblErrorconnexion.Font = new System.Drawing.Font("Garamond", 13.875F, System.Drawing.FontStyle.Bold);
+            this.lblErrorconnexion.ForeColor = System.Drawing.SystemColors.Menu;
+            this.lblErrorconnexion.Location = new System.Drawing.Point(0, 0);
+            this.lblErrorconnexion.Name = "lblErrorconnexion";
+            this.lblErrorconnexion.Size = new System.Drawing.Size(207, 42);
+            this.lblErrorconnexion.TabIndex = 0;
+            this.lblErrorconnexion.Text = "Notification";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 760);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.panelLogo1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogo1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox tbMdp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblErrorconnexion;
+    }
+}
+
---
+++ gsb/Form1.cs
@@ -0,0 +1,188 @@
+﻿using gbs_gcr1.config;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gbs_gcr1
+{
+    public partial class Form1 : Form
+    {
+        //private string[] donnees;
+        private string mot_de_passe;
+        private string login;
+        public string nom;
+        public string Type_EmployeC;
+        public string num_secteur;
+        public string idLocalisation;
+        public string idEmploye;
+        connexion cnn = new connexion();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void auth_user(string p_login,string p_mdp)
+        {
+            //connexion cnn = new connexion();
+            string connectionString = null;
+            connectionString = cnn.connexionstring(connectionString);
+            // Your query,
+            string query = "SELECT idEmploye,idTypeEmploye,nom,login,mdp,num_secteur,	idLocalisation FROM `employe`";
+
+            // Prepare the connection
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            // Let's do it !
+            try
+            {
+                // Open the database
+                databaseConnection.Open();
+
+                // Execute the query
+                reader = commandDatabase.ExecuteReader();
+
+                // All succesfully executed, now do something
+
+                // IMPORTANT : 
+                // If your query returns result, use the following processor :
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // As our database, the array will contain : ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
+                        // Do something with every received database ROW
+                        //string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                        if (p_login == reader["login"].ToString() && p_mdp == reader["mdp"].ToString())
+                        {
+                            Close();
+                            Type_EmployeC = reader["idTypeEmploye"].ToString();
+                            nom = reader["nom"].ToString();
+                            MessageBox.Show("Connexion reussie. Bonjour Mr/Mme "+nom);
+                            num_secteur = reader["num_secteur"].ToString(); 
+                            idLocalisation = reader["idLocalisation"].ToString();
+                            idEmploye = reader["idEmploye"].ToString();
+                            Home home = new Home();
+                            home.name = nom;
+                            home.idtypeemploye = Type_EmployeC;
+                            home.num_secteur = num_secteur;
+                            home.idLocalisation = idLocalisation;
+                            home.idEmploye = idEmploye;
+                            //Console.WriteLine(home);
+                            //home.Show();
+                            home.Show();
+                        }
+                        else
+                        {
+                            lblErrorconnexion.Text = "Identifiant ou mot de passe Incorrecte.";
+                            lblErrorconnexion.ForeColor = Color.Red;
+                        }
+                        //Console.WriteLine("nom_utilisateur: " + row[1] + "\tpseudo: " + row[2] + "\tmdp:" + row[3]);
+                    }
+                    //Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Pas de donnees");
+                }
+
+                // Finally close the connection
+                databaseConnection.Close();
+            }
+            catch (Exception)
+            {
+                // Show any error message.
+                //MessageBox.Show(ex.Message);
+                Console.WriteLine("Error");
+            }
+        }
+        private string hashage_mdp(string p_mdp,string p_typehashage_MAJUSCULE)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(p_mdp);
+
+            // need MD5 to calculate the hash
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName(""+ p_typehashage_MAJUSCULE + "")).ComputeHash(encodedPassword);
+
+            // string representation (similar to UNIX format)
+            string encoded = BitConverter.ToString(hash)
+               // without dashes
+               .Replace("-", string.Empty)
+               // make lowercase
+               .ToLower();
+            return encoded;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelid_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            login = tbLogin.Text;
+            mot_de_passe = tbMdp.Text;
+            mot_de_passe = hashage_mdp(mot_de_passe, "MD5");
+            //MessageBox.Show("Hashage MD5 du mot mdp:" + mot_de_passe);
+            auth_user(login, mot_de_passe);
+
+
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            //childForm.BringToFront();
+            childForm.Show();            
+            //lblTitle.Text = childForm.Text;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
